45538c742691404be2db4289ccb3e2631b6d4bef
Aman Netam <netama07@gmail.com>
2025-05-22 20:50:12 +0530

    Python brute force solution to Two_Sum problem.

---
+++ LeetCode/may22.py
@@ -0,0 +1,13 @@
+def two_sum(nums, target):
+    n = len(nums)
+    for i in range(n):
+        for j in range(i+1, n):
+            if nums[i] + nums[j] == target:
+                return [i, j]
+
+# nums = [1, 5, 2, 11, 15, 7]
+arr = list(map(int, input().split()))
+target = int(input())
+result = two_sum(arr, target)
+print(result)
+
