40ac15989b43e59deb733672df571ed109ef3a76
Aman Netam <netama07@gmail.com>
2025-05-22 21:09:23 +0530

    Updated the solution for Codechef problem [1316] — Combinatorics.

--- codechef/may21.cpp
+++ codechef/may21.cpp
@@ -1,38 +1,44 @@
 
-#include<iostream>
+#include <iostream>
 using namespace std;
 
 const int MOD = 998244353;
 
-int main() {
+int main()
+{
 	// your code goes here
 	int T;
-	cin>>T;
+	cin >> T;
 
 	while (T--)
 	{
 		int N;
-		cin>>N;
+		cin >> N;
 		vector<int> A(N);
 
-		for(int i=0; i< N; ++i){
-			cin>>A[i];
+		for (int i = 0; i < N; ++i)
+		{
+			cin >> A[i];
 		}
-	sort(A.begin(), A.end());
+		sort(A.begin(), A.end());
 
-	long long result = 1;
-	bool possible = true;
+		long long result = 1;
+		bool possible = true;
 
-	for (int i=0; i<N; ++i){
-		int options = A[i] - i;
-		if(options <= 0){
-			possible = false;
-			break;
+		for (int i = 0; i < N; ++i)
+		{
+			int options = A[i] - i;
+			if (options <= 0)
+			{
+				possible = false;
+				break;
+			}
+			result = (result * options) % MOD;
 		}
-		result = (result*options) % MOD;
-	}
-	if (!possible) cout<< 0<< "\n";
-	else cout<<result<<"\n";
+		if (!possible)
+			cout << 0 << "\n";
+		else
+			cout << result << "\n";
 	}
 	return 0;
 }
